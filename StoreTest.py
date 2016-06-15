e7dcd4de0bff1e25730d742645f1b6de61d5ea6b
Jacob-seguino <jacob.seguino@gmail.com>
2016-06-14 23:18:40 -0500

    fixing casting issue

--- StoreTest.py
+++ StoreTest.py
@@ -25,15 +25,16 @@ def addStore(name, low, high):
     low, high = lowToHigh(low, high)
     newStore = [name, low, high]
     store.append(newStore)
+    print "the new store list is:"
+    for i in store:
+        print i
 
 #for adding stores if you want to have the use put their input
 def addInputStores():
     name = raw_input("please enter a store name:")
     low = getRawNumber("low")
     high = getRawNumber("high")
-    low, high = lowToHigh(low, high)
-    newStore = [name, low, high]
-    store.append(newStore)
+    addStore(name, low, high)
 
 #for if you want the user to put in their own price range they are looking for
 def hiLowUserInput():
@@ -53,12 +54,12 @@ def lowToHigh(low, high):
 
 #check to see if the input is a number or not
 def getRawNumber(setWord):
-    num = raw_input("Please enter a {0} price:".format(setWord))
+    num = (int(raw_input("Please enter a {0} price:".format(setWord))))
     while not num.isdigit():
         num = raw_input("Sorry, that was not a number. Please, enter a number:")
     return num
 
- HiLow(2500, 5000)
+HiLow(2500, 5000)
 # HiLow(3000, 6000)
 # HiLow(200, 400)
 # HiLow(12000, 15000)
